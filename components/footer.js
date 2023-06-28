9e8f26a6c938f52390c1f3ed7ca0697c0648398c
ppd-pim~ <paphapi@kku.ac.th>
2023-06-28 11:28:55 +0700

    commit

--- components/footer.js
+++ components/footer.js
@@ -7,12 +7,12 @@ const footer = () => (
   <footer class="footer footer-black  footer-white ">
     <div class="container-fluid">
       <div class="row">
-        <div className="col-6">
+        <div className="col-6 pl-0">
           <a href="https://heart.kku.ac.th/" target="_blank">
             ศูนย์หัวใจสิริกิติ์ ภาคตะวันออกเฉียงเหนือ
           </a>
         </div>
-        <div className="col-6 text-end">
+        <div className="col-6 text-end pr-0">
           <span class="copyright">
             {/* <script>document.write(new Date().getFullYear())</script> */}
             ©2023, made
